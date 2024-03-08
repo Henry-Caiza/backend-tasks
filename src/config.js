52db4b63cd72ff7d6932d6778c40cc51aae6728d
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-07 21:17:55 -0500

    puerto

--- src/config.js
+++ src/config.js
@@ -2,3 +2,4 @@ import env from "dotenv"
 env.config()
 export const TOKEN_SECRET = process.env.JWT_SECRET
 export const MONGO_URL = process.env.DB_CONNECTION_URL
+export const PORT = process.env.PORT || 3000
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import app from "./app.js"
 import { connecDB } from "./db.js"
-
+import { PORT } from "./config.js"
 connecDB()
-app.listen(3000)
-console.log("listen on: ", 3000)
+app.listen(PORT)
+console.log("listen on: ", PORT)
