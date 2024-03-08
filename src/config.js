86cae3bce7c08af8f00310885cc9060026de53dc
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-07 20:30:57 -0500

    VARIABLES DE ENTORNO

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 import env from "dotenv"
 env.config()
-export const TOKEN_SECRET = process.env.TOKEN_SECRET
-export const MONGO_URL = process.env.PUBLIC_MONGO_URL
+export const TOKEN_SECRET = process.env.JWT_SECRET
+export const MONGO_URL = process.env.DB_CONNECTION_URL
