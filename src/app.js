fad37f9d0f5d420ba99d84170f7ffb9fb32dd7cc
Henry-Caiza <henrypcv1995@gmail.com>
2024-03-09 18:56:40 -0500

    acces

--- src/app.js
+++ src/app.js
@@ -8,13 +8,20 @@ import cors from 'cors'
 const app = express()
 
 app.use(cors({
-    //origin: 'https://back-tasks.vercel.app',
-    origin: 'https://taskscv.netlify.app',
-    //origin: 'http://localhost:5173',
+    origin: ['https://back-tasks.vercel.app',
+        'https://taskscv.netlify.app',
+        'http://localhost:5173'],
     credentials: true
 }))
 app.use(morgan('dev'))
 app.use(express.json())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://taskscv.netlify.app")
+    res.header("Access-Control-Allow-Credentials", "true")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+    next()
+})
 app.use(cookieParser())
 
 app.use("/api", authRoutes)
